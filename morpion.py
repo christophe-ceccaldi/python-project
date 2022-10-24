9a5f89bdc080cd22506e1e1106efb0d29ab70b0b
christophe-ceccaldi <114388420+christophe-ceccaldi@users.noreply.github.com>
2022-10-24 09:14:25 +0200

    "morpion not finish"

---
+++ morpion.py
@@ -0,0 +1,24 @@
+# créer la map du jeux
+map = ("---\n---\n---")
+print (map)
+# créer le compteur de point
+
+jouer = print (map)
+score = print()
+choice = input("Bonjour souhaites tu jouer ou voir les scores ?")
+if  choice(jouer):
+    ##
+else:
+    choice(score)
+    # proposition du choix et affichage du choix 
+
+nuser1 = input("Player1 comment t'appelles tu ? :")
+print ("Username 1 : ", nuser1)
+nuser2 = input("Player2 comment t'appelles tu ? :")
+print ("Username 2 : ", nuser2)
+# renseigner le nom des joueurs
+a = input(1 ,2, 3)
+b = input(1, 2, 3)
+def activgame ():
+
+
